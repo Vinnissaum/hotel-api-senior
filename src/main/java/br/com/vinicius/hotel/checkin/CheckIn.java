a3f11570890f503b7130c2db6c12dbe5bcaf80dd
Vinicius Braz <vinicius_lsb@live.com>
2023-09-13 12:38:32 -0300

    add entities

---
+++ src/main/java/br/com/vinicius/hotel/checkin/CheckIn.java
@@ -0,0 +1,34 @@
+package br.com.vinicius.hotel.checkin;
+
+import br.com.vinicius.hotel.guest.Guest;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "checkin")
+@Data
+@NoArgsConstructor
+public class CheckIn {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", updatable = false)
+    private UUID id;
+
+    @Column(nullable = false,  columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private LocalDateTime arrivalAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private LocalDateTime leftAt;
+
+    @ManyToOne
+    @JoinColumn(name = "guest_id")
+    private Guest guest;
+
+}
---
+++ src/main/java/br/com/vinicius/hotel/guest/Guest.java
@@ -0,0 +1,28 @@
+package br.com.vinicius.hotel.guest;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Guest {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", updatable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String phone;
+}
