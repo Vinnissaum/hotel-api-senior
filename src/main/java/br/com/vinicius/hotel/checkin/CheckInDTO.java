f90059f9a05f22d2d4ede972b8bb248ae1a9c8e2
Vinicius Braz <vinicius_lsb@live.com>
2023-09-13 21:27:10 -0300

    add DTO's

--- src/main/java/br/com/vinicius/hotel/checkin/CheckInDTO.java
+++ src/main/java/br/com/vinicius/hotel/checkin/CheckInDTO.java
@@ -1,2 +1,20 @@
-package br.com.vinicius.hotel.checkin;public class CheckInDTO {
+package br.com.vinicius.hotel.checkin;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+public class CheckInDTO {
+    private UUID id;
+
+    private LocalDateTime arrivalAt;
+
+    private LocalDateTime leftAt;
+
+    private UUID guestId;
+
 }
--- src/main/java/br/com/vinicius/hotel/guest/GuestDTO.java
+++ src/main/java/br/com/vinicius/hotel/guest/GuestDTO.java
@@ -1,2 +1,27 @@
-package br.com.vinicius.hotel.guest;public class GuestDTO {
+package br.com.vinicius.hotel.guest;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+public class GuestDTO {
+
+    private UUID id;
+
+    private String name;
+
+    private String document;
+
+    private String phone;
+
+    public GuestDTO(Guest entity) {
+        this.setId(entity.getId());
+        this.setName(entity.getName());
+        this.setDocument(entity.getDocument());
+        this.setPhone(entity.getPhone());
+    }
+
 }
